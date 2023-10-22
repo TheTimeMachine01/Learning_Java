284825552b1af2264fa9e8f3068705ce48ca7856
Ashish Verma <aniltechg2017@gmail.com>
2023-10-22 16:23:44 +0530

    Completed The Intro from W3Schools

--- src/W3School/Java_Home/learn1.java
+++ src/W3School/Java_Home/learn1.java
@@ -3,6 +3,6 @@ package W3School.Java_Home;
 public class learn1 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!");
     }
 }
